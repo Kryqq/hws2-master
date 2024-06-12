f5f331ae53becb9e370b59deb4b160cc50fb10f1
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-06-12 18:35:01 +0700

    kruto

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -1,58 +1,53 @@
-import React, {
-    ChangeEvent,
-    DetailedHTMLProps,
-    InputHTMLAttributes,
-} from 'react'
-import s from './SuperCheckbox.module.css'
+import React, { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import s from './SuperCheckbox.module.css';
 
 // тип пропсов обычного инпута
-type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>,
-    HTMLInputElement>
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 type SuperCheckboxPropsType = Omit<DefaultInputPropsType, 'type'> & {
-    onChangeChecked?: (checked: boolean) => void
-    spanClassName?: string
-}
+   onChangeChecked?: (checked: boolean) => void;
+   spanClassName?: string;
+};
 
-const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
-    {
-        onChange,
-        onChangeChecked,
-        className,
-        spanClassName,
-        children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
-        id,
+const SuperCheckbox: React.FC<SuperCheckboxPropsType> = ({
+   onChange,
+   onChangeChecked,
+   className,
+   spanClassName,
+   children, // в эту переменную попадёт текст, типизировать не нужно так как он затипизирован в React.FC
+   id,
+   checked,
+   ...restProps // все остальные пропсы попадут в объект restProps
+}) => {
+   const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+      console.log(e.currentTarget);
+      if (onChange) {
+         onChange(e);
+      }
+      if (onChangeChecked) {
+         onChangeChecked(!checked);
+      }
+   };
 
-        ...restProps // все остальные пропсы попадут в объект restProps
-    }
-) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // задачка на написание онченджа
+   const finalInputClassName = s.checkbox + (className ? ' ' + className : '');
 
-    }
+   return (
+      <label className={s.label}>
+         <input
+            checked={checked}
+            id={id}
+            type={'checkbox'}
+            onChange={onChangeCallback}
+            className={finalInputClassName}
+            {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
+         />
+         {children && (
+            <span id={id ? id + '-span' : undefined} className={s.spanClassName}>
+               {children}
+            </span>
+         )}
+      </label> // благодаря label нажатие на спан передастся в инпут
+   );
+};
 
-    const finalInputClassName = s.checkbox
-        + (className ? ' ' + className : '')
-
-    return (
-        <label className={s.label}>
-            <input
-                id={id}
-                type={'checkbox'}
-                onChange={onChangeCallback}
-                className={finalInputClassName}
-                {...restProps} // отдаём инпуту остальные пропсы если они есть (checked например там внутри)
-            />
-            {children && (
-                <span
-                    id={id ? id + '-span' : undefined}
-                    className={s.spanClassName}
-                >
-                    {children}
-                </span>
-            )}
-        </label> // благодаря label нажатие на спан передастся в инпут
-    )
-}
-
-export default SuperCheckbox
+export default SuperCheckbox;
